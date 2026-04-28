ea46c83553cbbb5016953c9076ce5d766fdc8402
brucelikeeat <138404664+brucelikeeat@users.noreply.github.com>
2026-04-27 19:34:15 -0700

    thread updated

--- src/thread.sv
+++ src/thread.sv
@@ -20,5 +20,63 @@ module thread #(
     output logic [DATA_WIDTH-1:0] result
 );
     // Instantiates: 1x fma
+    logic [DATA_WIDTH-1:0] accumulator, fma_result,
+    logic fma_valid_out, fma_ready,
+    logic [7:0] row, col;
+    fma#(
+        .DATA_WIDTH(DATA_WIDTH)
+    ) fma_inst (  
+        .clk       (clk),  
+        .rst       (rst),  
+        .a         (a_val),  
+        .b         (b_val),  
+        .c         (accumulator),  
+        .valid_in  (data_valid && en),  
+        .result    (fma_result),  
+        .valid_out (fma_valid_out),  
+        .ready     (fma_ready)  
+    );  
 
+    always_comb begin  
+        row = thread_id / N; // to know which row of the matrix
+        col = thread_id % N; // to know which column of the matrix
+    end
+    
+    always_comb begin 
+
+        // A[i][k] = base_addr_A + (i * N) + k  
+        addr_A = base_addr_A + (row * N) + j;
+
+        // B[k][j] = base_addr_B + (k*N)j;
+        addr_B = base_addr_B + (k * N) + col;
+
+        // C[i][j]= base_addr_C + (i * N) + j
+        addr_C = base_addr_C + (k * N) + col;
+    end
+
+    //ACCUMULATOR AND RESULT MANAGEMENT
+        // accumulator should store the partial sum across k iterations
+        // and when k = N -1 which is the last iteration, the accumulator holds the final C[i][j] value as the final result for output
+        
+    always @(posedge clk) begin
+        if(rst) begin  
+            accumulator <= '0;
+            result <= '0;
+            data_ready <= 1; // so its always ready for data
+        end else if (en) begin
+
+            //update the accumulator when fma completes
+
+            if(fma_valid_out) begin 
+                accumulator <= result;
+            end
+            
+
+            //output the final result on last iteration
+
+            if(k == N - 1 && fma_valid_out) begin
+                result <= fma_result;
+            end
+        end
+    end
 endmodule
